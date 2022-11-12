d43e096f6582f8afa9fc33caabe09c3bf8d1ad88
sabarigv <sabarigiri144@gmail.com>
2022-11-13 01:35:08 +0530

    final to-do-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let addtask = () => {
+    if (tasklist == "") {
+      alert("Enter your To-Do-List!!!", window.location.reload())
+    }
+    settodoList([
+      ...todoList,
+      { id: todoList.length + 1, name: `${tasklist}`, isDone: false },
+    ]);
+  };
+  let taskdone = (id) => {
+    let itemList = todoList.findIndex((obj) => obj.id === id);
+    settodoList([...todoList]);
+    if (todoList[itemList].isDone === false) {
+      todoList[itemList].isDone = true;
+    } else if (todoList[itemList].isDone == true) {
+      todoList[itemList].isDone = false;
+    }
+  };
+  let deletetask = (id) => {
+    let removetask = todoList.findIndex((obj) => obj.id === id);
+    todoList.splice(removetask, 1);
+    settodoList([...todoList]);
+  };
+
+  const [tasklist, setTaskList] = useState("");
+  const [todoList, settodoList] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <h1 className="todo">To-Do-List</h1>
+      <input className="ib" type="text" placeholder="Create your To-Do-List!" onChange={(e) => setTaskList(e.target.value)}></input>{" "}
+      <button onClick={addtask}>Add Task</button>
+      <ul>
+        {todoList.map((item) => {
+          return (
+            <li className={item.isDone ? "markdone" : ""}>{item.name} - <button onClick={() => taskdone(item.id)}>Done</button> - <button onClick={() => deletetask(item.id)}>Delete</button></li>
+          )
+        })
+        }
+      </ul>
     </div>
   );
 }
